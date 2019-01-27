b5de124767f7861624cfccea240e30af53f5fdd5
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-01-27 01:29:11 -0500

    app icon added

--- build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/health/atreacch/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/health/atreacch/R.java
@@ -9,10 +9,14 @@ package com.health.atreacch;
 
 public final class R {
   public static final class drawable {
-    public static final int launch_background=0x7f010000;
+    public static final int applogo=0x7f010000;
+    public static final int ic_launcher_background=0x7f010001;
+    public static final int launch_background=0x7f010002;
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f020000;
+    public static final int ic_launcher_foreground=0x7f020001;
+    public static final int ic_launcher_round=0x7f020002;
   }
   public static final class style {
     public static final int LaunchTheme=0x7f030000;
